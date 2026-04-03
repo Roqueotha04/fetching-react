26d1d123326669c75a12419dda8c5bccde57496f
roqueotha04 <roqueotha04@gmail.com>
2026-04-02 21:58:34 -0300

    finished fetching data

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,49 @@
 import { useEffect, useState } from 'react'
 import './App.css'
 
-const firstWord = 'papa';
+
 const RANDOM_FACT = `https://catfact.ninja/fact`
-const CAT_ENDPOINT_IMAGE_URL = `https://cataas.com/cat/says/${firstWord}?size=50&color=red&json=true`
+//const CAT_ENDPOINT_IMAGE_URL = `https://cataas.com/cat/says/${firstWord}?size=50&color=red&json=true`
 
 function App() {
   const[fact, setFact] = useState('cualquier cosa')
+  const [image, setImage] = useState()
 
+
+  //efectos tienen una sola responsabiliad
   useEffect(()=>{
     fetch(RANDOM_FACT)
       .then(res => res.json()) 
-      .then(data =>setFact(data.fact))
-
+//      .then(data =>setFact(data.fact)) Abajo hace lo mismo pero con desestructuración
+      .then(data =>{
+          const {fact} = data;
+          setFact(fact);
+      })
   },[])
+
+useEffect(()=>{
+      const firstWord = fact.split(' ')[0];
+      console.log(firstWord);
+
+      fetch(`https://cataas.com/cat/says/${firstWord}?size=50&color=red&json=true`)
+        .then(res => res.json())
+       .then(response =>{
+         setImage(response.url)
+        })
+        .catch((err)=>{
+          console.log(err);
+        })
+}, [fact])
 
   return (
     <main>
       <h1>App de gatos</h1>
+
+      <section>
       <p>{fact}</p>
+      {image && <img src={image} alt='cat'></img>}
+      </section>
+      
     </main>
   )
 }
